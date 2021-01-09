c5a667e6c4c43d625031ac6810610c33b5e8ff12
Amr Magdy <amagdy195@yahoo.com>
2021-01-09 22:34:47 +0200

    lec 3

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -8,8 +8,8 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var viewModel: EmojiMemoryGame
+struct EmojiMemoryGameView: View {
+   @ObservedObject  var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack {
@@ -17,12 +17,10 @@ struct ContentView: View {
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
                 }
-                .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .padding()
         .foregroundColor(Color.orange)
-        .font(viewModel.cards.count < 5 ? .largeTitle : .subheadline)
     }
 }
 
@@ -30,6 +28,12 @@ struct CardView:View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
@@ -39,11 +43,21 @@ struct CardView:View {
                 RoundedRectangle(cornerRadius: 10.0).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+
     }
+
+    func fontSize(for size: CGSize) -> CGFloat {
+        return min(size.width, size.height) * 0.75
+    }
+
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    let fontScaleFactor: CGFloat = 0.75
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
